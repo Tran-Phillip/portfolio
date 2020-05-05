7b24709b6125887f0f4e5fb534efecb7c6d7e0fd
Phillip Tran <pntran@ucdavis.edu>
2020-05-04 22:22:23 -0700

    Updates

---
+++ precache-manifest.45b2bcc2f8a128d2c91110ed17060a29.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e48042d37a6ad01f162634bd29cbefc8",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4f1007637757092783be",
+    "url": "/portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "00d8a4d7d965b922b8ca",
+    "url": "/portfolio/static/js/2.932798e9.chunk.js"
+  },
+  {
+    "revision": "7e6aa1aa6ff0f40561c8431c2bdf24b9",
+    "url": "/portfolio/static/js/2.932798e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f1007637757092783be",
+    "url": "/portfolio/static/js/main.31a66362.chunk.js"
+  },
+  {
+    "revision": "e7fea7fb795252924974",
+    "url": "/portfolio/static/js/runtime-main.dcc1eea5.js"
+  },
+  {
+    "revision": "bfd5ae026878bd5825e81ccfa2d38c91",
+    "url": "/portfolio/static/media/enter.bfd5ae02.png"
+  },
+  {
+    "revision": "cdbaeb6a416c0440a423d0da583a7b8a",
+    "url": "/portfolio/static/media/firstName.cdbaeb6a.png"
+  },
+  {
+    "revision": "7c1ae4aeaa2e9cfb971e137f47912d00",
+    "url": "/portfolio/static/media/lastName.7c1ae4ae.png"
+  },
+  {
+    "revision": "aec2a3f04f9592d1a6188baefef15ad5",
+    "url": "/portfolio/static/media/logo.aec2a3f0.png"
+  }
+]);
